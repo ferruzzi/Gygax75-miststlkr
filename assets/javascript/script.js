0320a209a9a664d6153748aaa9e72962f35e9bbd
miststlkr <miststlkr@gmail.com>
2020-05-02 22:00:19 -0700

    Adding points of interest and tinkering with formatting

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,1 +1,1 @@
-document.getElementById("project_title").innerHTML = "Just Another Untitled Zoph Project";
+document.getElementById("project_title").innerHTML = "Just Another Untitled and unfinished Zoph Project";
