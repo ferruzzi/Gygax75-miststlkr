09ce92a36e4f71874d17a417c7526cc4441cbb6f
miststlkr <miststlkr@gmail.com>
2020-05-02 21:40:37 -0700

    Adding points of interest and tinkering with formatting

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,2 +1,1 @@
-document.getElementById("project_title").innerHTML = "An Untitled Zoph Project";
-document.getElementById("forkme_banner").style.visibility = "hidden"";
+document.getElementById("project_title").innerHTML = "Untitled Zoph Project";
